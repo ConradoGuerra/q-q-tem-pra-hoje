24dfc2d69b51c934e8a8c56dc58673b6c50d937d
conrado.guerra <conrado.guerra@ilek.fr>
2025-01-26 20:46:03 +0100

    Improve Recipe usecase

--- domain/recipe/service_test.go
+++ domain/recipe/service_test.go
@@ -1,6 +1,7 @@
 package recipe
 
 import (
+	"errors"
 	"q-q-tem-pra-hoje/domain/ingredient"
 	"testing"
 
@@ -13,41 +14,91 @@ type Recipe struct {
 }
 
 type RecipeManager interface {
-	AddRecipe(recipe Recipe)
+	AddRecipe(recipe Recipe) error
 }
 
-// Injection
+func NewRecipe(name string, ingredients []ingredient.Ingredient) (Recipe, error) {
+	recipe := Recipe{Name: name, Ingredients: ingredients}
+	if err := recipe.Validate(); err != nil {
+		return Recipe{}, err
+	}
+	return recipe, nil
+}
+
 type RecipeService struct {
-	recipeRepository (RecipeManager)
+	RecipeManager
 }
 
-func NewService(r RecipeManager) *RecipeService {
-	return &RecipeService{r}
+func NewRecipeService(m RecipeManager) *RecipeService {
+	return &RecipeService{RecipeManager: m}
 }
 
-func (r RecipeService) AddRecipe(recipe Recipe) {
-	r.recipeRepository.AddRecipe(recipe)
+func (r *Recipe) Validate() error {
+	if r.Name == "" {
+		return errors.New("recipe name cannot be empty")
+	}
+	if len(r.Ingredients) == 0 {
+		return errors.New("recipe must have at least one ingredient")
+	}
+	return nil
 }
 
-type InMemoryRecipeRepository struct {
+func (s *RecipeService) AddRecipe(recipe Recipe) error {
+	return s.RecipeManager.AddRecipe(recipe)
+}
+
+type InMemoryRecipeManager struct {
 	Recipes []Recipe
 }
 
-func (i *InMemoryRecipeRepository) AddRecipe(recipe Recipe) {
-	i.Recipes = append(i.Recipes, recipe)
+func NewInMemoryRecipeManager() *InMemoryRecipeManager {
+	return &InMemoryRecipeManager{Recipes: []Recipe{}}
+}
+
+func (m *InMemoryRecipeManager) AddRecipe(recipe Recipe) error {
+	m.Recipes = append(m.Recipes, recipe)
+	return nil
 }
 
 func TestAddRecipe(t *testing.T) {
-	t.Run("it should add a recipe", func(t *testing.T) {
-		recipe := Recipe{
-			Name:        "Rice",
-			Ingredients: []ingredient.Ingredient{{Name: "onions", MeasureType: "units", Quantity: 1}},
-		}
-		recipeRepository := &InMemoryRecipeRepository{}
-		recipeService := NewService(recipeRepository)
+	t.Run("It should add a valid recipe", func(t *testing.T) {
 
-		recipeService.AddRecipe(Recipe{Name: "Rice", Ingredients: []ingredient.Ingredient{{Name: "onions", MeasureType: "units", Quantity: 1}}})
+		expectedRecipe, err := NewRecipe("Rice", []ingredient.Ingredient{
+			{Name: "Onion", MeasureType: "unit", Quantity: 1},
+			{Name: "Rice", MeasureType: "mg", Quantity: 500},
+			{Name: "Garlic", MeasureType: "unit", Quantity: 2}})
+		inMemoryRecipeManager := NewInMemoryRecipeManager()
+		recipeService := NewRecipeService(inMemoryRecipeManager)
 
-		assert.Contains(t, recipeRepository.Recipes, recipe)
+		recipeService.AddRecipe(expectedRecipe)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedRecipe, inMemoryRecipeManager.Recipes[len(inMemoryRecipeManager.Recipes)-1])
+	})
+
+	t.Run("It should return an error for an invalid name", func(t *testing.T) {
+		invalidRecipe, err := NewRecipe("", []ingredient.Ingredient{ // Empty name
+			{Name: "Onion", MeasureType: "unit", Quantity: 1},
+			{Name: "Rice", MeasureType: "mg", Quantity: 500},
+			{Name: "Garlic", MeasureType: "unit", Quantity: 2},
+		})
+		manager := NewInMemoryRecipeManager()
+		service := NewRecipeService(manager)
+
+		service.AddRecipe(invalidRecipe)
+		assert.Error(t, err)
+		assert.Equal(t, "recipe name cannot be empty", err.Error())
+		assert.Equal(t, Recipe{}, invalidRecipe)
+	})
+
+	t.Run("It should return an error for invalid ingredients", func(t *testing.T) {
+		invalidRecipe, err := NewRecipe("Rice", []ingredient.Ingredient{}) // No ingredients
+		manager := NewInMemoryRecipeManager()
+		service := NewRecipeService(manager)
+
+		service.AddRecipe(invalidRecipe)
+		assert.Error(t, err)
+		assert.Equal(t, "recipe must have at least one ingredient", err.Error())
+		assert.Equal(t, Recipe{}, invalidRecipe)
+
 	})
 }
