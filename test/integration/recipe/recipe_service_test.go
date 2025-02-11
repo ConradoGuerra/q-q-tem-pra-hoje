97b74aef4bc1f7bd8a1d4777b4d0b252302caa72
conrado.guerra <conrado.guerra@ilek.fr>
2025-02-11 20:45:11 +0100

    Implement GetAll recipes

--- test/integration/recipe/recipe_service_test.go
+++ test/integration/recipe/recipe_service_test.go
@@ -97,7 +97,53 @@ func (m PostgreSQLRecipeManager) AddRecipe(recipe recipe.Recipe) error {
 	return nil
 }
 func (m PostgreSQLRecipeManager) GetAllRecipes() []recipe.Recipe {
-	return []recipe.Recipe{}
+	rows, err := m.Query(`SELECT 
+                          r.name, 
+                          i.name, 
+                          i.measure_type, 
+                          i.quantity 
+                        FROM recipes r 
+                          JOIN recipes_ingredients i ON r.id = i.recipe_id`)
+
+	if err != nil {
+		fmt.Errorf("Error on query recipe: %v", err)
+	}
+
+	recipeMap := make(map[string]*recipe.Recipe)
+
+	for rows.Next() {
+		var recipeName string
+		var ingredientRetrieved ingredient.Ingredient
+
+		err := rows.Scan(&recipeName, &ingredientRetrieved.Name, &ingredientRetrieved.MeasureType, &ingredientRetrieved.Quantity)
+		if err != nil {
+			fmt.Errorf("Failed to scan row: %v", err)
+		}
+
+		// Check if the recipe already exists in the map
+		if r, exists := recipeMap[recipeName]; exists {
+			// Append the ingredient to the existing recipe
+			r.Ingredients = append(r.Ingredients, ingredientRetrieved)
+		} else {
+			// Create a new recipe and add it to the map and slice
+			newRecipe := &recipe.Recipe{ // Use a pointer to the recipe
+				Name:        recipeName,
+				Ingredients: []ingredient.Ingredient{ingredientRetrieved},
+			}
+			recipeMap[recipeName] = newRecipe
+		}
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Errorf("Error iterating rows: %v", err)
+	}
+
+	var recipesRetrieved []recipe.Recipe
+	for _, r := range recipeMap {
+		recipesRetrieved = append(recipesRetrieved, *r) 
+	}
+
+	return recipesRetrieved
 }
 
 func TestAddRecipe(t *testing.T) {
@@ -160,5 +206,75 @@ func TestAddRecipe(t *testing.T) {
 		err := service.AddRecipe(newRecipe)
 		assert.Error(t, err)
 		assert.Contains(t, err.Error(), "Failed to insert recipe: pq: duplicate key value violates unique constraint")
+	})
+}
+
+func TestCreateRecommendations(t *testing.T) {
+	db := setupDatabase(t)
+
+	t.Cleanup(func() {
+		teardownDatabase(db, t)
+	})
+
+	recipes := []recipe.Recipe{
+		{Name: "Rice with Onion and Garlic", Ingredients: []ingredient.Ingredient{
+			{Name: "Onion", MeasureType: "unit", Quantity: 1},
+			{Name: "Rice", MeasureType: "mg", Quantity: 500},
+			{Name: "Garlic", MeasureType: "unit", Quantity: 2},
+		}},
+		{Name: "Rice with Garlic", Ingredients: []ingredient.Ingredient{
+			{Name: "Rice", MeasureType: "mg", Quantity: 500},
+			{Name: "Garlic", MeasureType: "unit", Quantity: 2},
+		}},
+		{Name: "Rice with Onion", Ingredients: []ingredient.Ingredient{
+			{Name: "Onion", MeasureType: "unit", Quantity: 1},
+			{Name: "Rice", MeasureType: "mg", Quantity: 500},
+		}},
+		{Name: "Fries", Ingredients: []ingredient.Ingredient{
+			{Name: "Potato", MeasureType: "unit", Quantity: 2},
+		}},
+	}
+
+	// Insert recipes and their ingredients
+	for _, recipe := range recipes {
+		// Insert the recipe
+		var recipeID int
+		err := db.QueryRow("INSERT INTO recipes (name) VALUES ($1) RETURNING id;", recipe.Name).Scan(&recipeID)
+		if err != nil {
+			t.Fatalf("Failed to insert recipe %q: %v", recipe.Name, err)
+		}
+
+		// Insert ingredients for the recipe
+		for _, ing := range recipe.Ingredients {
+			_, err := db.Exec(`
+                INSERT INTO recipes_ingredients (recipe_id, name, measure_type, quantity)
+                VALUES ($1, $2, $3, $4)
+                ON CONFLICT (recipe_id, name) DO NOTHING;
+            `, recipeID, ing.Name, ing.MeasureType, ing.Quantity)
+			if err != nil {
+				t.Fatalf("Failed to insert ingredient %q for recipe %q: %v", ing.Name, recipe.Name, err)
+			}
+		}
+	}
+	recipeManager := PostgreSQLRecipeManager{db}
+
+	service := recipe.NewRecipeService(recipeManager)
+
+	t.Run("should create the recommendations", func(t *testing.T) {
+
+		availableIngredients := []ingredient.Ingredient{
+			{Name: "Onion", MeasureType: "unit", Quantity: 1},
+			{Name: "Rice", MeasureType: "mg", Quantity: 500},
+		}
+
+		recommendations := service.CreateRecommendations(&availableIngredients)
+		expectedRecommendations := []recipe.Recommendation{
+			{Recommendation: 1, Recipe: recipes[2]},
+			{Recommendation: 2, Recipe: recipes[0]},
+			{Recommendation: 3, Recipe: recipes[1]},
+			{Recommendation: 4, Recipe: recipes[3]},
+		}
+
+		assert.Equal(t, expectedRecommendations, recommendations)
 	})
 }
