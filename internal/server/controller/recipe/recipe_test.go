e13a50bd1240e30f430ba62fa99133995da1b895
conrado.guerra <conrado.guerra@ilek.fr>
2025-03-23 19:08:21 +0100

    Add StatusCode and new test case for invalid input

--- internal/server/controller/recipe/recipe_test.go
+++ internal/server/controller/recipe/recipe_test.go
@@ -23,31 +23,36 @@ func (rc RecipeController) Add(w http.ResponseWriter, r *http.Request) {
 		Ingredients []ingredient.Ingredient `json:"ingredients"`
 	}
 
-	json.NewDecoder(r.Body).Decode(&recipeDTO)
+	if err := json.NewDecoder(r.Body).Decode(&recipeDTO); err != nil {
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Invalid request body",
+		})
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	rc.recipeProvider.Add(recipe.Recipe(recipeDTO))
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 
 }
 
 type MockedRecipeService struct {
 	recipes recipe.Recipe
-	add     func(recipe recipe.Recipe) recipe.Recipe
 }
 
 func (mrs *MockedRecipeService) Add(rec recipe.Recipe) {
-  mrs.recipes = rec
+	mrs.recipes = rec
 
 }
 
 func TestRecipeController_Add(t *testing.T) {
 
-	createdRecipe, _ := recipe.NewRecipe("Rice", []ingredient.Ingredient{
-		{Name: "Onion", MeasureType: "unit", Quantity: 1},
-	})
-
-	t.Run("should add a recipe", func(t *testing.T) {
-		service := MockedRecipeService{add: func(recipe recipe.Recipe) recipe.Recipe {
-			return recipe
-		}}
+	t.Run("should return 201 and add a recipe", func(t *testing.T) {
+		createdRecipe, _ := recipe.NewRecipe("Rice", []ingredient.Ingredient{
+			{Name: "Onion", MeasureType: "unit", Quantity: 1},
+		})
+		service := MockedRecipeService{}
 
 		controller := RecipeController{recipeProvider: &service}
 		w := httptest.NewRecorder()
@@ -58,5 +63,24 @@ func TestRecipeController_Add(t *testing.T) {
 		controller.Add(w, r)
 
 		assert.Equal(t, createdRecipe, service.recipes)
+		assert.Equal(t, http.StatusCreated, w.Code)
+		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+
+	})
+
+	t.Run("should return 400 and message error when the input is not valid", func(t *testing.T) {
+		service := MockedRecipeService{}
+
+		controller := RecipeController{recipeProvider: &service}
+		w := httptest.NewRecorder()
+
+		requestBody := `{"name":, "ingredients": [{"measureType":"","quantity":1}]}`
+
+		r := httptest.NewRequest("POST", "/recipe", bytes.NewBufferString(requestBody))
+		controller.Add(w, r)
+
+		assert.JSONEq(t, `{"message":"Invalid request body"}`, w.Body.String())
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
 	})
 }
