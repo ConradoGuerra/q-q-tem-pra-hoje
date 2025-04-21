bfeab0502aacb103bdf6d87c40a49c4a9ea750a0
conrado.guerra <conrado.guerra@ilek.fr>
2025-04-21 16:04:52 +0200

    Add html page

--- internal/app/server.go
+++ internal/app/server.go
@@ -9,12 +9,29 @@ import (
 	ingredientController "q-q-tem-pra-hoje/internal/server/controller/ingredient"
 	recipeController "q-q-tem-pra-hoje/internal/server/controller/recipe"
 	ingredientService "q-q-tem-pra-hoje/internal/service/ingredient"
-	recipeService"q-q-tem-pra-hoje/internal/service/recipe"
+	recipeService "q-q-tem-pra-hoje/internal/service/recipe"
 )
 
 type Server struct {
 	server *http.Server
 	db     *sql.DB
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func NewServer() (*Server, error) {
@@ -24,20 +41,22 @@ func NewServer() (*Server, error) {
 	}
 
 	ism := postgres.NewIngredientStorageManager(db)
-  rm := postgres.NewRecipeManager(db)
+	rm := postgres.NewRecipeManager(db)
 	is := ingredientService.NewService(&ism)
-  rs := recipeService.NewRecipeService(rm)
+	rs := recipeService.NewRecipeService(rm)
 	ic := ingredientController.NewIngredientController(is)
-	rc:= recipeController.NewRecipeController(is, rs)
+	rc := recipeController.NewRecipeController(is, rs)
 
 	mux := http.NewServeMux()
 	mux.Handle("/ingredient", ic)
 	mux.Handle("/recipe", rc)
 
+	handler := corsMiddleware(mux)
+
 	return &Server{
 		server: &http.Server{
 			Addr:    ":8080",
-			Handler: mux,
+			Handler: handler,
 		},
 		db: db,
 	}, nil
