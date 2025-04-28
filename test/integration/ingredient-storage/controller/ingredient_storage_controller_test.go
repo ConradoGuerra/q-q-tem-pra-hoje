010118bc6c3c9b53bb29c9497ad1d625a4d8f74a
conrado.guerra <conrado.guerra@ilek.fr>
2025-04-28 20:06:07 +0200

    Add getIngedients to integration test

--- test/integration/ingredient-storage/controller/ingredient_storage_controller_test.go
+++ test/integration/ingredient-storage/controller/ingredient_storage_controller_test.go
@@ -2,6 +2,8 @@ package integration_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"q-q-tem-pra-hoje/internal/domain/ingredient"
@@ -39,4 +41,43 @@ func TestIngredientStorageController_Add(t *testing.T) {
 
 		assert.Empty(t, resp.Body)
 	})
+}
+
+func TestIngredientStorageController_GetAll(t *testing.T) {
+	t.Run("should retrieve all ingredients from the repository", func(t *testing.T) {
+		repository := in_memory_repository.NewIngredientStorageManager()
+		ingredients := []ingredient.Ingredient{
+			{Name: "Salt", MeasureType: "unit", Quantity: 1},
+			{Name: "Pepper", MeasureType: "unit", Quantity: 2},
+		}
+		for _, ing := range ingredients {
+			repository.Ingredients = append(repository.Ingredients, ing)
+		}
+
+		svc := service.NewService(&repository)
+		ctrl := controller.NewIngredientController(svc)
+
+		server := httptest.NewServer(ctrl)
+		resp, err := http.Get(server.URL + "/ingredient")
+		if err != nil {
+			t.Fatalf("failed to retrieve ingredient: %v", err)
+		}
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatalf("failed to read response body: %v", err)
+		}
+
+		var responseIngredients []ingredient.Ingredient
+		err = json.Unmarshal(body, &responseIngredients)
+		if err != nil {
+			t.Fatalf("failed to unmarshal response: %v", err)
+		}
+
+		assert.Len(t, responseIngredients, 2)
+		assert.Equal(t, ingredients, responseIngredients)
+	})
+
 }
