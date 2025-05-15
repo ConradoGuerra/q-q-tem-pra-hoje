86becae91cbf5ee266fc0c88458207c26f8d71d0
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-15 19:56:03 +0200

    Add e2e delete test case

--- test/e2e/api/recipe/recipe_controller_test.go
+++ test/e2e/api/recipe/recipe_controller_test.go
@@ -81,11 +81,11 @@ func TestRecipeController_Add(t *testing.T) {
 
 		resp, err := http.Post(ts.URL+"/recipe", "application/json", bytes.NewBufferString(body))
 		if err != nil {
-			t.Fatalf("Failed to get ingredients: %v", err)
+			t.Fatalf("failed to get ingredients: %v", err)
 		}
 
 		if resp.StatusCode != http.StatusCreated {
-			t.Errorf("Expected status %d, got %d", http.StatusCreated, resp.StatusCode)
+			t.Errorf("expected status %d, got %d", http.StatusCreated, resp.StatusCode)
 		}
 	})
 }
@@ -104,7 +104,7 @@ func TestRecipeController_GetRecipes(t *testing.T) {
 
 	defer ts.Close()
 
-	t.Run("should create a recipe", func(t *testing.T) {
+	t.Run("should retrieve the recipes", func(t *testing.T) {
 
 		query := `INSERT INTO ingredients_storage(name, measure_type, quantity)
             VALUES ($1, $2, $3), ($4, $5, $6);`
@@ -169,5 +169,87 @@ func TestRecipeController_GetRecipes(t *testing.T) {
 		var recipesFound []recipe.Recipe
 		err = json.Unmarshal(body, &recipesFound)
 		assert.Equal(t, recipes, recipesFound)
+	})
+}
+
+func TestRecipeController_Delete(t *testing.T) {
+	db, teardown := setupDatabase(t)
+	handler := app.NewHandler(db)
+
+	t.Cleanup(func() {
+		teardown()
+		db.Close()
+
+	})
+
+	ts := httptest.NewServer(handler)
+
+	defer ts.Close()
+
+	t.Run("should delete a recipe", func(t *testing.T) {
+
+		query := `INSERT INTO ingredients_storage(name, measure_type, quantity)
+            VALUES ($1, $2, $3), ($4, $5, $6);`
+		_, err := db.Exec(query, "Onion", "unit", 1, "Rice", "mg", 500)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		recipes := []recipe.Recipe{
+			{Name: "Rice with Onion and Garlic", Ingredients: []ingredient.Ingredient{
+				{Name: "Onion", MeasureType: "unit", Quantity: 1},
+				{Name: "Rice", MeasureType: "mg", Quantity: 500},
+				{Name: "Garlic", MeasureType: "unit", Quantity: 2},
+			}},
+			{Name: "Rice with Garlic", Ingredients: []ingredient.Ingredient{
+				{Name: "Rice", MeasureType: "mg", Quantity: 500},
+				{Name: "Garlic", MeasureType: "unit", Quantity: 2},
+			}},
+			{Name: "Rice with Onion", Ingredients: []ingredient.Ingredient{
+				{Name: "Onion", MeasureType: "unit", Quantity: 1},
+				{Name: "Rice", MeasureType: "mg", Quantity: 500},
+			}},
+			{Name: "Fries", Ingredients: []ingredient.Ingredient{
+				{Name: "Potato", MeasureType: "unit", Quantity: 2},
+			}},
+		}
+
+		for _, recipe := range recipes {
+			var recipeId int
+			err := db.QueryRow("INSERT INTO recipes (name) VALUES ($1) RETURNING id;", recipe.Name).Scan(&recipeId)
+			if err != nil {
+				t.Fatalf("failed to insert recipe %q: %v", recipe.Name, err)
+			}
+
+			for _, ing := range recipe.Ingredients {
+				_, err := db.Exec(`
+                INSERT INTO recipes_ingredients (recipe_id, name, measure_type, quantity)
+                VALUES ($1, $2, $3, $4)
+                ON CONFLICT (recipe_id, name) DO NOTHING;
+            `, recipeId, ing.Name, ing.MeasureType, ing.Quantity)
+				if err != nil {
+					t.Fatalf("failed to insert ingredient %q for recipe %q: %v", ing.Name, recipe.Name, err)
+				}
+			}
+		}
+
+		req, err := http.NewRequest(http.MethodDelete, ts.URL+"/recipe?id=2", bytes.NewBufferString(`{}`))
+		if err != nil {
+			t.Fatalf("failed to create request: %v", err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("failed to delete a recipe: %v", err)
+		}
+
+		if resp.StatusCode != http.StatusNoContent {
+			t.Errorf("expected status %d, got %d", http.StatusOK, resp.StatusCode)
+		}
+
 	})
 }
